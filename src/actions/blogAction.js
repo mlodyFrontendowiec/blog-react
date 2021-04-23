095c529f71e08a800ab20e38cb31f894a2ee4cfa
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-04-23 20:49:03 +0200

    Change addAction

--- src/actions/blogAction.js
+++ src/actions/blogAction.js
@@ -2,12 +2,12 @@ export const ADD_POST = "ADD_POST";
 export const DELETE_POST = "DELETE_POST";
 export const EDIT_POST = "EDIT_POST";
 
-export const addPost = ({ title, content, date }) => ({
+export const addPost = ({ title, content }) => ({
   type: ADD_POST,
   payload: {
     title,
     content,
-    date,
+    date: Date.now(),
     id: Math.floor(Math.random() * 321423242332),
   },
 });
