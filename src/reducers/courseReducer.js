dca0b6c97c6aac558b1b268fb5b60669b4f5d3ad
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-05-01 08:28:35 +0200

    Remove export default

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -4,11 +4,11 @@ export const courseReducer = (state = [], { type, payload }) => {
   switch (type) {
     case ADD_COURSE:
       return [...state, payload];
+
     case DELETE_COURSE:
       return [...state.filter((item) => item.id !== payload.id)];
+
     default:
       return state;
   }
 };
-
-export default courseReducer;
