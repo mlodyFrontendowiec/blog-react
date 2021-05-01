e5359903d9382b285aa08f8eb340475098be6c3a
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-05-01 08:49:52 +0200

    Remove console.log

--- src/reducers/challengeReducer.js
+++ src/reducers/challengeReducer.js
@@ -4,8 +4,10 @@ export const challengesReducer = (state = [], { type, payload }) => {
   switch (type) {
     case ADD_CHALLENGES:
       return [...state, payload];
+
     case DELETE_CHALLENGES:
       return [...state.filter((item) => item.id !== payload.id)];
+
     default:
       return state;
   }
